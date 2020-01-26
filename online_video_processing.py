ba8b6223deffb505a2b3768db9565934c65bf63b
Leo Adlakha <leoa.co18@nsut.ac.in>
2020-01-26 22:57:32 +0530

    Finalized Pothole_Detection Model but further training reqd

--- online_video_processing.py
+++ online_video_processing.py
@@ -15,7 +15,7 @@ def process_stream(options, camera_index):
     capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
     while True:
-        # stime = time.time()
+        stime = time.time()
         ret, frame = capture.read()
         results = tfnet.return_predict(frame)
         if ret and len(results) > 0:
@@ -28,20 +28,20 @@ def process_stream(options, camera_index):
             driver that there is a pothole ahead, hence he should drive slow.
             '''
 
-            # for color, result in zip(colors, results):
-            #     top_left = (result['topleft']['x'], result['topleft']['y'])
-            #     bottom_right = (result['bottomright']['x'],
-            #                     result['bottomright']['y'])
-            #     label = result['label']
-            #     confidence = result['confidence']
-            #     text = '{}: {:.0f}%'.format(label, confidence*100)
-            #     frame = cv2.rectangle(frame, top_left, bottom_right, color, 5)
-            #     frame = cv2.putText(frame, text, top_left,
-            #                         cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
-            # cv2.imshow('frame', frame)
-            # print('FPS => {:.1f}'.format(1 / (time.time() - stime)))
-        # if cv2.waitKey(1) & 0xff == ord('q'):
-        #     break
+            for color, result in zip(colors, results):
+                top_left = (result['topleft']['x'], result['topleft']['y'])
+                bottom_right = (result['bottomright']['x'],
+                                result['bottomright']['y'])
+                label = result['label']
+                confidence = result['confidence']
+                text = '{}: {:.0f}%'.format(label, confidence*100)
+                frame = cv2.rectangle(frame, top_left, bottom_right, color, 5)
+                frame = cv2.putText(frame, text, top_left,
+                                    cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+            cv2.imshow('frame', frame)
+            print('FPS => {:.1f}'.format(1 / (time.time() - stime)))
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
 
     capture.release()
     cv2.destroyAllWindows()
