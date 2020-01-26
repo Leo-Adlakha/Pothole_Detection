a0967832afd89217019f27632b41fbd66b0d396e
Leo Adlakha <leoa.co18@nsut.ac.in>
2020-01-26 15:51:53 +0530

    Code ready to be added to Raspi

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
-import cv2
-from darkflow.net.build import TFNet
-import matplotlib.pyplot as plt
-%config InlineBackend.figure_format = 'svg'
+import online_video_processing
 
 options = {
     'model': "cfg/tiny-yolo-voc-1c.cfg",
     'load': 1000,
-    'threshold': 0.16
+    'threshold': 0.2
 }
 
-tfNet = TFNet(options)
+online_video_processing.process_stream(options, 0)
 
-img = cv2.imread('demo2.jpg', cv2.IMREAD_COLOR)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-result = tfNet.return_predict(img)
+'''
+Below are the instructions for an image processing for potholes and 
+drawing boxes around it.
+'''
 
-top_left = [(result[i]['topleft']['x'], result[i]['topleft']['y'])
-            for i in range(len(result))]
-bottom_right = [(result[i]['bottomright']['x'], result[i]
-                 ['bottomright']['y']) for i in range(len(result))]
-labels = [result[i]['label'] for i in range(len(result))]
-for i in range(len(top_left)):
-    img = cv2.rectangle(img, top_left[i], bottom_right[i], (0, 255, 0), 7)
-    img = cv2.putText(img, labels[i], top_left[i],
-                      cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 0, 0), 4)
-plt.imshow(img)
-plt.show()
+# tfNet = TFNet(options)
+
+# img = cv2.imread('demo2.jpg', cv2.IMREAD_COLOR)
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# result = tfNet.return_predict(img)
+
+# top_left = [(result[i]['topleft']['x'], result[i]['topleft']['y'])
+#             for i in range(len(result))]
+# bottom_right = [(result[i]['bottomright']['x'], result[i]
+#                  ['bottomright']['y']) for i in range(len(result))]
+# labels = [result[i]['label'] for i in range(len(result))]
+# for i in range(len(top_left)):
+#     img = cv2.rectangle(img, top_left[i], bottom_right[i], (0, 255, 0), 7)
+#     img = cv2.putText(img, labels[i], top_left[i],
+#                       cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 0, 0), 4)
+# plt.imshow(img)
+# plt.show()
